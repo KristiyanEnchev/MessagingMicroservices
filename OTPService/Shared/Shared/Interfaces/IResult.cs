5e7d3c8251c3a4ec065ef0c90e509b65b519f40e
Kiko <kristiqnenchevv@gmail.com>
2024-01-11 12:07:32 +0200

    Add Result to OTP Service

---
+++ OTPService/Shared/Shared/Interfaces/IResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.Interfaces
+{
+    using System.Collections.Generic;
+
+    public interface IResult<T>
+    {
+        bool Success { get; set; }
+        T Data { get; set; }
+        List<string> Errors { get; set; }
+    }
+}
