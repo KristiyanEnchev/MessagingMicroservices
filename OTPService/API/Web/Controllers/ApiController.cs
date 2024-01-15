8f9b32151f9c4cd9852b15cbedf12ac8398c5b53
Kiko <kristiqnenchevv@gmail.com>
2024-01-15 12:05:55 +0200

    Add Controllers and pakecges to OTP Web

---
+++ OTPService/API/Web/Controllers/ApiController.cs
@@ -0,0 +1,18 @@
+﻿namespace Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using MediatR;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class ApiController : ControllerBase
+    {
+        protected const string Id = "{id}";
+        protected const string PathSeparator = "/";
+
+        private ISender _mediator = null!;
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
