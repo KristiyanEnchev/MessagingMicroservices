05363a265ef09ece21add58734159c242607586f
Kiko <kristiqnenchevv@gmail.com>
2024-01-17 14:12:20 +0200

    Add Current user Service to SMS Service Web

---
+++ SMSService/API/Web/Services/CurrentUser.cs
@@ -0,0 +1,20 @@
+﻿namespace Web.Services
+{
+    using System.Security.Claims;
+
+    using Microsoft.AspNetCore.Http;
+
+    using Application.Interfaces;
+
+    public class CurrentUser : IUser
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUser(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string? Id => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
