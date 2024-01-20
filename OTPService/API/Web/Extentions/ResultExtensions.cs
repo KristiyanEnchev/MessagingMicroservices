bf9de5f781a881be1e69fe2d0640e0f5ea814cdf
Kiko <kristiqnenchevv@gmail.com>
2024-01-20 12:09:10 +0200

    Add Result Extention to all API's

---
+++ OTPService/API/Web/Extentions/ResultExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace Web.Extentions
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using Shared;
+
+    public static class ResultExtensions
+    {
+        public static async Task<ActionResult> ToActionResult<TData>(this Task<Result<TData>> resultTask)
+        {
+            var result = await resultTask;
+
+            var response = new
+            {
+                success = result.Success,
+                data = result.Data,
+                errors = !result.Success ? result.Errors : null
+            };
+
+            if (!result.Success)
+            {
+                if (result.Errors != null)
+                {
+                    return new BadRequestObjectResult(response);
+                }
+                return new BadRequestObjectResult("Unexpected Error occured");
+            }
+
+            else if (result.Data == null)
+            {
+                return new NoContentResult();
+            }
+
+            return new OkObjectResult(response);
+        }
+    }
+}
