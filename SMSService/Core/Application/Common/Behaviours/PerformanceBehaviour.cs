b8c42badbf38ed6f1a4f7c55efb5e3914fffbef1
Kiko <kristiqnenchevv@gmail.com>
2024-01-22 14:09:56 +0200

    Add validation Behavior to SMS Api

---
+++ SMSService/Core/Application/Common/Behaviours/PerformanceBehaviour.cs
@@ -0,0 +1,50 @@
+﻿namespace Application.Common.Behaviours
+{
+    using System.Diagnostics;
+
+    using Microsoft.Extensions.Logging;
+
+    using MediatR;
+
+    using Application.Interfaces;
+
+    public class PerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        private readonly Stopwatch _timer;
+        private readonly ILogger<TRequest> _logger;
+        private readonly IUser _user;
+
+        public PerformanceBehaviour(
+            ILogger<TRequest> logger,
+            IUser user)
+        {
+            _timer = new Stopwatch();
+
+            _logger = logger;
+            _user = user;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            _timer.Start();
+
+            var response = await next();
+
+            _timer.Stop();
+
+            var elapsedMilliseconds = _timer.ElapsedMilliseconds;
+
+            if (elapsedMilliseconds > 500)
+            {
+                var requestName = typeof(TRequest).Name;
+                var userId = _user.Id ?? string.Empty;
+                var userName = string.Empty;
+
+                _logger.LogWarning("Authentication.Api Long Running Request: {Name} ({ElapsedMilliseconds} milliseconds) {@UserId} {@Request}",
+                    requestName, elapsedMilliseconds, userId, userName, request);
+            }
+
+            return response;
+        }
+    }
+}
