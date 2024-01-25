94bdf193b90d64995ebc36c81e3477ba9e268c3c
Kiko <kristiqnenchevv@gmail.com>
2024-01-25 12:10:23 +0200

    Add Startups and confugure packeges

---
+++ EmailService/Infrastructure/Infrastructure/Startup.cs
@@ -0,0 +1,58 @@
+﻿namespace Infrastructure
+{
+    using System.Net;
+    using System.Text;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.IdentityModel.Tokens;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+
+    using MediatR;
+
+    using Application.Interfaces;
+
+    using Domain.Common;
+    using Domain.Entities.Identity;
+    using Domain.Common.Interfaces;
+
+    using Infrastructure.Services;
+    using Infrastructure.Identity.Services;
+    using Infrastructure.Account.Services;
+
+    using Persistence.Contexts;
+    using Persistence.Constants;
+
+    using Models;
+
+    public static class Startup
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddServices()
+                .AddConfigurations(configuration);
+
+            return services;
+        }
+
+        private static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services
+                .AddTransient<IMediator, Mediator>();
+
+            return services;
+        }
+
+       
+        private static IServiceCollection AddConfigurations(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
---
+++ OTPService/API/Web/Startup.cs
@@ -0,0 +1,61 @@
+﻿namespace Web
+{
+    using System.Reflection;
+
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Routing;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using Application;
+    using Application.Interfaces;
+
+    using Web.Services;
+    using Web.Extentions.Swagger;
+
+    using Infrastructure;
+
+    using Web.Extentions.Middleware;
+
+    public static class Startup
+    {
+        public static IServiceCollection AddWeb(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddHttpContextAccessor();
+            services.AddControllers().AddApplicationPart(Assembly.GetExecutingAssembly()).AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+            });
+
+            services.AddApplication();
+            services.AddInfrastructure(config);
+            services.AddPersistence(config);
+
+            services.AddSwaggerDocumentation();
+
+            services.AddScoped<IUser, CurrentUser>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseWeb(this IApplicationBuilder builder)
+        {
+            builder.UseSwaggerDocumentation()
+                    .UseStaticFiles()
+                    .UseHttpsRedirection()
+                    .UseErrorHandler()
+                    .UseRouting()
+                    .UseAuthentication()
+                    .UseAuthorization();
+
+            return builder;
+        }
+
+        public static IEndpointRouteBuilder MapEndpoints(this IEndpointRouteBuilder builder)
+        {
+            builder.MapControllers().RequireAuthorization();
+
+            return builder;
+        }
+    }
+}
