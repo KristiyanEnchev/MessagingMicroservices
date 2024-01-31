3b5091a4b68313c681d47e57ba1f7a1ee97577bb
Kiko <kristiqnenchevv@gmail.com>
2024-01-31 14:11:01 +0200

    Tie up the Api gateway

---
+++ ApiGateways/ApiGateways/Extentions/ServiceCollectionExtention.cs
@@ -0,0 +1,97 @@
+﻿namespace ApiGateways.Extentions
+{
+    using System.Text;
+
+    using Microsoft.OpenApi.Models;
+    using Microsoft.IdentityModel.Tokens;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+    using Ocelot.DependencyInjection;
+
+    public static class ServiceCollectionExtention
+    {
+        public static IServiceCollection AddCustomAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var secret = configuration
+            .GetSection("Secrets")
+            .GetValue<string>("Secret")!;
+
+            var key = Encoding.UTF8.GetBytes(secret);
+
+            services
+                .AddAuthentication(authentication =>
+                {
+                    authentication.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    authentication.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+
+            return services;
+        }
+
+        public static IServiceCollection AddOcelotWithSwagger(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddOcelot(configuration);
+            services.AddCacheManager();
+            services.AddSwaggerForOcelot(configuration,
+                (o) =>
+                {
+                    o.GenerateDocsForAggregates = true;
+                    o.GenerateDocsDocsForGatewayItSelf(opt =>
+                    {
+                        opt.GatewayDocsTitle = "ApiGataway";
+                        opt.GatewayDocsOpenApiInfo = new()
+                        {
+                            Version = "v1",
+                            Title = "ApiGataway.Api",
+                            Description = "Provide basic ApiGataway functionality.",
+                            Contact = new OpenApiContact
+                            {
+                                Name = "Kris" ?? "",
+                                Email = "kristiqnenchevv@gmail.com" ?? "",
+                                Url = null
+                            }
+                        };
+                        opt.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+                        {
+                            Name = "Authorization",
+                            In = ParameterLocation.Header,
+                            Type = SecuritySchemeType.Http,
+                            Scheme = JwtBearerDefaults.AuthenticationScheme,
+                            BearerFormat = "JWT",
+                            Description = "Input your Bearer token directly in the field to access this API",
+                        });
+                        opt.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                          {
+                             {
+                                new OpenApiSecurityScheme
+                                {
+                                    Reference = new OpenApiReference
+                                    {
+                                        Type = ReferenceType.SecurityScheme,
+                                        Id = JwtBearerDefaults.AuthenticationScheme,
+                                    },
+                                    Scheme = JwtBearerDefaults.AuthenticationScheme,
+                                    Name = JwtBearerDefaults.AuthenticationScheme,
+                                    In = ParameterLocation.Header,
+                                }, new List<string>()
+                             },
+                          });
+                    });
+                });
+
+            return services;
+        }
+    }
+}
--- ApiGateways/ApiGateways/Program.cs
+++ ApiGateways/ApiGateways/Program.cs
@@ -1,9 +1,7 @@
 namespace ApiGateway
 {
-    using Microsoft.AspNetCore.Authentication.JwtBearer;
-    using Microsoft.OpenApi.Models;
+    using ApiGateways.Extentions;
 
-    using Ocelot.DependencyInjection;
     using Ocelot.Middleware;
 
     public class Program
@@ -12,80 +10,26 @@ namespace ApiGateway
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddAuthentication(authentication =>
-            {
-                authentication.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                authentication.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            });
-            builder.Services.AddAuthorization();
+            builder.Services
+                .AddCustomAuthentication(builder.Configuration)
+                .AddAuthorization()
+                .AddControllers();
 
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services
+                .AddEndpointsApiExplorer()
+                .AddSwaggerGen();
 
             builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true);
-            builder.Services.AddOcelot(builder.Configuration);
-            builder.Services.AddCacheManager();
-            builder.Services.AddSwaggerForOcelot(builder.Configuration,
-                (o) =>
-                {
-                    o.GenerateDocsForAggregates = true;
-                    o.GenerateDocsDocsForGatewayItSelf(opt =>
-                    {
-                        opt.GatewayDocsTitle = "ApiGataway";
-                        opt.GatewayDocsOpenApiInfo = new()
-                        {
-                            Version = "v1",
-                            Title = "ApiGataway.Api",
-                            Description = "Provide basic ApiGataway functionality.",
-                            Contact = new OpenApiContact
-                            {
-                                Name = "Kris" ?? "",
-                                Email = "kristiqnenchevv@gmail.com" ?? "",
-                                Url = null
-                            }
-                        };
-                        opt.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
-                        {
-                            Name = "Authorization",
-                            In = ParameterLocation.Header,
-                            Type = SecuritySchemeType.Http,
-                            Scheme = JwtBearerDefaults.AuthenticationScheme,
-                            BearerFormat = "JWT",
-                            Description = "Input your Bearer token directly in the field to access this API",
-                        });
-                        opt.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                          {
-                             {
-                                new OpenApiSecurityScheme
-                                {
-                                    Reference = new OpenApiReference
-                                    {
-                                        Type = ReferenceType.SecurityScheme,
-                                        Id = JwtBearerDefaults.AuthenticationScheme,
-                                    },
-                                    Scheme = JwtBearerDefaults.AuthenticationScheme,
-                                    Name = JwtBearerDefaults.AuthenticationScheme,
-                                    In = ParameterLocation.Header,
-                                }, new List<string>()
-                             },
-                          });
-                    });
-                });
+
+            builder.Services.AddOcelotWithSwagger(builder.Configuration);
 
             var app = builder.Build();
 
+            app.UseSwagger();
+            app.UseHttpsRedirection();
+            app.MapControllers();
             app.UseAuthentication();
             app.UseAuthorization();
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.MapControllers();
 
             app.UseSwaggerForOcelotUI(opt =>
             {
