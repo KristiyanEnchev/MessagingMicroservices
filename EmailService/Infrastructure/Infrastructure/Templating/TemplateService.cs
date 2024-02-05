b16a7986eec983200817d7f6fd8ef6f2e2769adc
Kiko <kristiqnenchevv@gmail.com>
2024-02-05 12:13:40 +0200

    Add initial template service method and crate template data model

--- EmailService/Infrastructure/Infrastructure/Templating/TemplateService.cs
+++ EmailService/Infrastructure/Infrastructure/Templating/TemplateService.cs
@@ -1,6 +1,23 @@
 ﻿namespace Infrastructure.Templating
 {
+    using System.Web;
+
+    using Models;
+
     public class TemplateService
     {
+        public async Task<string> ReplaceDataOnTemplate(string templateContent, IEnumerable<TemplateData> placeholders)
+        {
+            templateContent = HttpUtility.HtmlDecode(templateContent);
+
+            if (placeholders != null)
+                foreach (var placeholder in placeholders)
+                {
+                    templateContent = templateContent.Replace("{{" + placeholder.Field + "}}", placeholder.Value);
+                }
+
+            return templateContent;
+        }
+
     }
 }
---
+++ EmailService/Infrastructure/Models/TemplateData.cs
@@ -0,0 +1,9 @@
+﻿namespace Models
+{
+    public class TemplateData
+    {
+        public string? Field { get; set; }
+
+        public string? Value { get; set; }
+    }
+}
