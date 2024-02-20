4a060cfe653209db5bdb01914943a6843392f5de
Kiko <kristiqnenchevv@gmail.com>
2024-02-20 12:06:11 +0200

    Add Hangfire extention

---
+++ SMSService/API/Web/Extentions/Hangfire/HangfireExtention.cs
@@ -0,0 +1,54 @@
+﻿namespace Web.Extentions.Hangfire
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using global::Hangfire;
+    using global::Hangfire.Redis.StackExchange;
+
+    using Newtonsoft.Json;
+
+    public static class HangfireExtention
+    {
+        public static IServiceCollection AddHangfireConfigurations(this IServiceCollection services, IConfiguration configuration) 
+        {
+            var redisConnection = configuration.GetConnectionString("Redis");
+
+
+            services.AddHangfire(configuration => configuration
+                .UseRedisStorage(redisConnection, new RedisStorageOptions
+                {
+                    Prefix = "hangfire:",
+                })
+                .UseSerilogLogProvider()
+                .UseMediatR());
+
+            services.AddHangfireServer();
+
+            return services;
+        }
+
+        private static void UseMediatR(this IGlobalConfiguration configuration)
+        {
+            var jsonSettings = new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.All
+            };
+            configuration.UseSerializerSettings(jsonSettings);
+        }
+
+        public static IApplicationBuilder UseHangfireConfiguration(this IApplicationBuilder app) 
+        {
+            app.UseHangfireDashboard("/hangfire", new DashboardOptions
+            {
+                Authorization = new[] 
+                {
+                    new HangfireCustomBasicAuthenticationFilter("Admin", "password")
+                }
+            });
+
+            return app;
+        }
+    }
+}
