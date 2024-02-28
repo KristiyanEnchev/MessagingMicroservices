ec7af59838c92f7e49b61e50fbfe8cb1f9117f02
Kiko <kristiqnenchevv@gmail.com>
2024-02-28 12:04:55 +0200

    Add Templated SMS handler

---
+++ SMSService/Core/Application/Handlers/Twilio/Commands/SendTemplateSMSCommand.cs
@@ -0,0 +1,46 @@
+﻿namespace Application.Handlers.SMTP.Commands
+{
+    using Microsoft.Extensions.DependencyInjection;
+
+    using MediatR;
+
+    using AutoMapper;
+
+    using Shared;
+
+    using Models.SMS;
+    using Application.Interfaces.SMS;
+    using Hangfire;
+
+    public class SendTemplateSMSCommand : TemplateSmsMessage, IRequest<Result<string>>
+    {
+        public virtual void Mapping(Profile mapper)
+        {
+            mapper.CreateMap<SendTemplateSMSCommand, TemplateSmsMessage>().ReverseMap();
+            mapper.CreateMap<SendTemplateSMSCommand, SmsMessage>().ReverseMap();
+        }
+
+        public class SendTemplateEmailCommandHandler : IRequestHandler<SendTemplateSMSCommand, Result<string>>
+        {
+            private readonly IServiceProvider _serviceProvider;
+
+            public SendTemplateEmailCommandHandler(IServiceProvider serviceProvider)
+            {
+                _serviceProvider = serviceProvider;
+            }
+
+            public async Task<Result<string>> Handle(SendTemplateSMSCommand request, CancellationToken cancellation)
+            {
+                var smsService = request.SmsProvider!.ToLower() switch
+                {
+                    "twilio" => _serviceProvider.GetRequiredService<ITwilioSMSService>(),
+                    _ => throw new ArgumentException("Invalid sms provider specified."),
+                };
+
+                BackgroundJob.Enqueue(() => smsService.SendSMSWithLocalTemplate(request));
+
+                return Result<string>.SuccessResult("Template sms send to request queued.");
+            }
+        }
+    }
+}
