d306586f4be8e13e83d48696d3ce37b60eb69361
Kiko <kristiqnenchevv@gmail.com>
2024-02-29 14:10:29 +0200

    Add validation for templated SMS

---
+++ SMSService/Core/Application/Handlers/Twilio/Commands/SendTemplateEmailValidator.cs
@@ -0,0 +1,24 @@
+﻿namespace Application.Handlers.SMTP.Commands
+{
+    using FluentValidation;
+
+    public class SendTemplateSMSValidator : AbstractValidator<SendTemplateSMSCommand>
+    {
+        public SendTemplateSMSValidator() 
+        {
+            RuleFor(u => u.SmsProvider)
+                .NotEmpty()
+                .WithMessage("Sms Provider is required.");
+
+            RuleFor(u => u.TemplateName)
+                .NotEmpty()
+                .WithMessage("TemplateName is required.");
+
+            RuleFor(u => u.To)
+                .NotNull()
+                .WithMessage("Recipient cannot be null.")
+                .NotEmpty()
+                .WithMessage("Recipient cannot be empty.");
+        }
+    }
+}
