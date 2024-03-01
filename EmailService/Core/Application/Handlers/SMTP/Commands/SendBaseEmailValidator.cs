7288ff876a10951c3f6a89bb3403ef28f8795b78
Kiko <kristiqnenchevv@gmail.com>
2024-03-01 14:05:05 +0200

    Add validator for the base email handler

---
+++ EmailService/Core/Application/Handlers/SMTP/Commands/SendBaseEmailValidator.cs
@@ -0,0 +1,32 @@
+﻿namespace Application.Handlers.SMTP.Commands
+{
+    using FluentValidation;
+
+    public class SendBaseEmailValidator : AbstractValidator<SendBaseEmailCommand>
+    {
+        public SendBaseEmailValidator() 
+        {
+            RuleFor(u => u.EmailProvider)
+                .NotEmpty()
+                .WithMessage("Email Provider is required.");
+
+            RuleFor(u => u.Body)
+                .NotNull()
+                .WithMessage("Body is required.")
+                .NotEmpty()
+                .WithMessage("Body is required.");
+
+            RuleFor(u => u.To)
+                .NotNull()
+                .WithMessage("Recipient list cannot be null.")
+                .NotEmpty()
+                .WithMessage("Recipient list cannot be empty.");
+
+            RuleFor(u => u.Subject)
+                .NotNull()
+                .WithMessage("Subject cannot be null.")
+                .NotEmpty()
+                .WithMessage("Subject be empty.");
+        }
+    }
+}
