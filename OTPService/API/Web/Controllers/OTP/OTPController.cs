bcf86d3a52c0f4bd2775a12554bf4bb386609101
Kiko <kristiqnenchevv@gmail.com>
2024-03-14 12:01:50 +0200

    Add Generate OTP Controller

---
+++ OTPService/API/Web/Controllers/OTP/OTPController.cs
@@ -0,0 +1,20 @@
+﻿namespace Web.Controllers.OTP
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using Swashbuckle.AspNetCore.Annotations;
+
+    using Application.Handlers.OneTimePin.Command;
+
+    using Web.Extentions;
+
+    public class OTPController : ApiController
+    {
+        [HttpPost(nameof(Generate))]
+        [SwaggerOperation("Generate One Time Pin.", "Uses Cryptography to generate One Time Pin and store's it in cache for validation.")]
+        public async Task<IActionResult> Generate(OneTimePinGenerateCommand command)
+        {
+            return await Mediator.Send(command).ToActionResult();
+        }
+    }
+}
