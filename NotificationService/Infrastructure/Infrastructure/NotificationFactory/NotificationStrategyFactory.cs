4e4685541b40350e3b57b9c3d72258f572a4903a
Kiko <kristiqnenchevv@gmail.com>
2024-03-20 12:11:38 +0200

    Add notification Strategy factory method

---
+++ NotificationService/Infrastructure/Infrastructure/NotificationFactory/NotificationStrategyFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace Infrastructure.NotificationFactory
+{
+    using Application.Interfaces.Notification;
+
+    public class NotificationStrategyFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public NotificationStrategyFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public INotificationStrategy GetStrategy(string type)
+        {
+            var strategyName = $"{type}NotificationStrategy";
+            var strategy = _serviceProvider.GetService(typeof(INotificationStrategy)) as INotificationStrategy;
+            if (strategy == null)
+            {
+                throw new ArgumentException($"Unsupported notification type: {type}");
+            }
+            return strategy;
+        }
+    }
+}
