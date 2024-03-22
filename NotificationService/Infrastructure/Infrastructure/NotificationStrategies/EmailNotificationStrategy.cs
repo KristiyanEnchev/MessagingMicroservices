508dcebba1cb68446cf79ac4b9b5d13762ffdbc8
Kiko <kristiqnenchevv@gmail.com>
2024-03-22 14:11:08 +0200

    Add Base Email Notification Strategy

---
+++ NotificationService/Infrastructure/Infrastructure/NotificationStrategies/EmailNotificationStrategy.cs
@@ -0,0 +1,16 @@
+﻿namespace Infrastructure.NotificationStrategies
+{
+    using Application.Interfaces.Notification;
+
+    using Models.Notification;
+
+    using Shared;
+
+    public class EmailNotificationStrategy : INotificationStrategy
+    {
+        public async Task<Result<string>> ExecuteAsync(NotificationRequest request)
+        {
+            return Result<string>.SuccessResult("Email sent successfully");
+        }
+    }
+}
