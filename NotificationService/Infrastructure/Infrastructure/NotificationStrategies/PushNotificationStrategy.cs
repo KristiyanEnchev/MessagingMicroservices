753160650ff1502eeaa6f3ec275734223abb7cc1
Kiko <kristiqnenchevv@gmail.com>
2024-03-24 12:03:11 +0200

    Add push and scheduled notification strategies

---
+++ NotificationService/Infrastructure/Infrastructure/NotificationStrategies/PushNotificationStrategy.cs
@@ -0,0 +1,16 @@
+﻿namespace Infrastructure.NotificationStrategies
+{
+    using Application.Interfaces.Notification;
+
+    using Models.Notification;
+
+    using Shared;
+
+    public class PushNotificationStrategy : INotificationStrategy
+    {
+        public async Task<Result<string>> ExecuteAsync(NotificationRequest request)
+        {
+            return Result<string>.SuccessResult("Successfull notification send.");
+        }
+    }
+}
---
+++ NotificationService/Infrastructure/Infrastructure/NotificationStrategies/ScheduledNotificationStrategy.cs
@@ -0,0 +1,16 @@
+﻿namespace Infrastructure.NotificationStrategies
+{
+    using Application.Interfaces.Notification;
+
+    using Models.Notification;
+
+    using Shared;
+
+    public class ScheduledNotificationStrategy : INotificationStrategy
+    {
+        public async Task<Result<string>> ExecuteAsync(NotificationRequest request)
+        {
+            return Result<string>.SuccessResult("Successfull scheduled notification send.");
+        }
+    }
+}
