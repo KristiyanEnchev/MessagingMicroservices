8feb4ef27ee6e5e211452f1cb083b76f69bd513b
Kiko <kristiqnenchevv@gmail.com>
2024-03-28 14:07:10 +0200

    Add Hangfire Extention

---
+++ NotificationService/API/Web/Extentions/Hangfire/HangfireExtention.cs
@@ -0,0 +1,55 @@
+﻿namespace Web.Extentions.Hangfire
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using global::Hangfire;
+    using global::Hangfire.Redis.StackExchange;
+
+    using Newtonsoft.Json;
+
+    public static class HangfireExtention
+    {
+        public static IServiceCollection AddHangfireConfigurations(this IServiceCollection services, IConfiguration configuration) 
+        {
+            var redisConnection = configuration.GetConnectionString("Redis");
+
+            services.AddSingleton<NotificationJobActivator, NotificationJobActivator>();
+
+            services.AddHangfire(configuration => configuration
+                .UseRedisStorage(redisConnection, new RedisStorageOptions
+                {
+                    Prefix = "hangfire:Notification:",
+                })
+                .UseSerilogLogProvider()
+                .UseMediatR());
+
+            services.AddHangfireServer();
+
+            return services;
+        }
+
+        private static void UseMediatR(this IGlobalConfiguration configuration)
+        {
+            var jsonSettings = new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.All
+            };
+            configuration.UseSerializerSettings(jsonSettings);
+        }
+
+        public static IApplicationBuilder UseHangfireConfiguration(this IApplicationBuilder app) 
+        {
+            app.UseHangfireDashboard("/hangfire", new DashboardOptions
+            {
+                Authorization = new[] 
+                {
+                    new HangfireCustomBasicAuthenticationFilter("Admin", "password")
+                }
+            });
+
+            return app;
+        }
+    }
+}
