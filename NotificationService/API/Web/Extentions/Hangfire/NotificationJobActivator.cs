731a1599c1bb763228fe13a04b5eeca8d63c35b6
Kiko <kristiqnenchevv@gmail.com>
2024-03-28 12:05:57 +0200

    Add Job Activator

---
+++ NotificationService/API/Web/Extentions/Hangfire/NotificationJobActivator.cs
@@ -0,0 +1,39 @@
+﻿namespace Web.Extentions.Hangfire
+{
+    using Microsoft.Extensions.DependencyInjection;
+
+    using global::Hangfire;
+    using global::Hangfire.Server;
+
+    public class NotificationJobActivator : JobActivator
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public NotificationJobActivator(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory ?? throw new ArgumentNullException(nameof(scopeFactory));
+        }
+
+        public override JobActivatorScope BeginScope(PerformContext context)
+        {
+            var scope = _scopeFactory.CreateScope();
+            return new SimpleScope(scope.ServiceProvider);
+        }
+
+        private class SimpleScope : JobActivatorScope
+        {
+            private readonly IServiceProvider _serviceProvider;
+
+            public SimpleScope(IServiceProvider serviceProvider)
+            {
+                _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+            }
+
+            public override object Resolve(Type type)
+            {
+                return _serviceProvider.GetService(type) ??
+                       throw new InvalidOperationException($"Requested service of type {type.Name} was not found.");
+            }
+        }
+    }
+}
