65516a8dffda04ee20dfc4099b6d6363a4709fe8
Kiko <kristiqnenchevv@gmail.com>
2024-03-31 14:12:51 +0300

    Add Notification Sender interface

---
+++ NotificationService/Core/Application/Interfaces/Notification/INotificationSender.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Interfaces.Notification
+{
+    using Shared.Notification;
+
+    public interface INotificationSender
+    {
+        Task BroadcastAsync(INotificationMessage notification, CancellationToken cancellationToken);
+        Task BroadcastAsync(INotificationMessage notification, IEnumerable<string> excludedConnectionIds, CancellationToken cancellationToken);
+        Task SendToGroupAsync(INotificationMessage notification, string group, CancellationToken cancellationToken);
+        Task SendToUsersAsync(INotificationMessage notification, IEnumerable<string> userIds, CancellationToken cancellationToken);
+    }
+}
