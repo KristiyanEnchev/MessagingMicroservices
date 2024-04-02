285459c63d23a53c9fa60b6fd632a3b29bf26e66
Kiko <kristiqnenchevv@gmail.com>
2024-04-02 14:04:55 +0300

    Update Strategies

---
+++ NotificationService/Infrastructure/Infrastructure/NotificationStrategies/EventNotificationStrategy.cs
@@ -0,0 +1,27 @@
+﻿//namespace Infrastructure.NotificationStrategies
+//{
+//    using Application.Interfaces.Notification;
+
+//    using Models.Notification;
+
+//    using Shared;
+
+//    public class EventNotificationStrategy : INotificationStrategy
+//    {
+//        private readonly IEventBus _eventBus;
+
+//        public EventNotificationStrategy(IEventBus eventBus)
+//        {
+//            _eventBus = eventBus;
+//        }
+
+//        public Task ExecuteAsync(NotificationRequest request)
+//        {
+//            // Logic to format the message and publish it to the client_notification_exchange
+//            var @event = new ClientNotificationEvent { Message = request.Message };
+//            _eventBus.Publish(@event, request); // Adjust Publish method to accommodate this use case
+
+//            return Task.CompletedTask;
+//        }
+//    }
+//}
