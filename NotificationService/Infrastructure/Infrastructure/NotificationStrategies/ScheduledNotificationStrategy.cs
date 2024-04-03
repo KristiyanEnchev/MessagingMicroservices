47bb74213ae398ec9ac06b8b2b2b47f3dffdbb10
Kiko <kristiqnenchevv@gmail.com>
2024-04-03 14:03:19 +0300

    Add shceduled Notification

---
+++ NotificationService/Infrastructure/Infrastructure/NotificationStrategies/ScheduledNotificationStrategy.cs
@@ -0,0 +1,57 @@
+﻿namespace Infrastructure.NotificationStrategies
+{
+    using Microsoft.Extensions.Logging;
+
+    using Hangfire;
+
+    using Application.Interfaces.Notification;
+
+    using Models.Notification;
+
+    using Shared;
+
+    public class ScheduledNotificationStrategy : INotificationStrategy
+    {
+        private readonly INotificationSender _sender;
+        private readonly ILogger<ScheduledNotificationStrategy> _logger;
+
+        public ScheduledNotificationStrategy(INotificationSender sender, ILogger<ScheduledNotificationStrategy> logger)
+        {
+            _sender = sender;
+            _logger = logger;
+        }
+
+        public async Task<Result<string>> ExecuteAsync(NotificationRequest request, CancellationToken cancellationToken)
+        {
+            var jobIdStart = BackgroundJob.Schedule(() => _sender
+                    .SendToGroupAsync(
+                        new PushNotification
+                        {
+                            Message = request.Message,
+                            Activate = request.Activate,
+                        },
+                        request.ClientId!,
+                        cancellationToken
+                    ),
+                    request.StartDate
+                );
+
+            var jobIdEnd = BackgroundJob.Schedule(() => _sender
+                   .SendToGroupAsync(
+                       new PushNotification
+                       {
+                           Message = request.Message,
+                           Activate = !request.Activate,
+                       },
+                       request.ClientId!,
+                       cancellationToken
+                   ),
+                   request.EndDate
+               );
+
+            _logger.LogInformation($"Successfull scheduled notification start:{request.StartDate} end:{request.EndDate}");
+
+            return Result<string>.SuccessResult("Successfull scheduled notification send.");
+        }
+    }
+}
