55379a04b5a0a2d10af97307b8a0da5d79962737
Kiko <kristiqnenchevv@gmail.com>
2024-04-05 14:00:33 +0300

    Add Notification Hub

---
+++ NotificationService/Infrastructure/Infrastructure/SignalR/NotificationHub.cs
@@ -0,0 +1,53 @@
+﻿namespace Infrastructure.SignalR
+{
+    using Microsoft.AspNetCore.SignalR;
+    using Microsoft.Extensions.Logging;
+
+    public class NotificationHub : Hub
+    {
+        private readonly ILogger<NotificationHub> _logger;
+
+        public NotificationHub(ILogger<NotificationHub> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            var clientId = httpContext.Request.Query["clientId"];
+
+            if (!string.IsNullOrEmpty(clientId))
+            {
+                var groupName = $"Group-{clientId}";
+                await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+                _logger.LogInformation($"A client connected to NotificationHub: {Context.ConnectionId} and joined group {groupName}");
+            }
+            else
+            {
+                _logger.LogWarning($"A client connected without specifying targetClient or clientId: {Context.ConnectionId}");
+            }
+
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var httpContext = Context.GetHttpContext();
+            var clientId = httpContext.Request.Query["clientId"];
+
+            if (!string.IsNullOrEmpty(clientId))
+            {
+                var groupName = $"Group-{clientId}";
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+                _logger.LogInformation($"A client disconnected to NotificationHub: {Context.ConnectionId} and joined group {groupName}");
+            }
+            else
+            {
+                _logger.LogWarning($"A client disconnected without specifying targetClient or clientId: {Context.ConnectionId}");
+            }
+
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
