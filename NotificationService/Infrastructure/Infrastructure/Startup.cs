037248e13b5d21f6be84dc969d7085de08871d5f
Kiko <kristiqnenchevv@gmail.com>
2024-04-07 14:11:08 +0300

    Update packeges and Infrastructure Start up

--- NotificationService/Infrastructure/Infrastructure/Startup.cs
+++ NotificationService/Infrastructure/Infrastructure/Startup.cs
@@ -5,28 +5,35 @@
 
     using MediatR;
 
+    using global::RabbitMQ.Client;
+
+    using Infrastructure.RabbitMQ;
     using Infrastructure.NotificationFactory;
     using Infrastructure.NotificationStrategies;
 
     using Application.Interfaces.Notification;
+
+    using Models.Notification;
+    using Infrastructure.SignalR;
+    using Microsoft.AspNetCore.Connections;
 
     public static class Startup
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services
-                .AddServices()
-                .AddConfigurations(configuration);
+                .AddConfigurations(configuration)
+                .AddServices(configuration);
 
             return services;
         }
 
-        private static IServiceCollection AddServices(this IServiceCollection services)
+        private static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
         {
-            services
-                .AddTransient<IMediator, Mediator>();
-
-            services.AddScoped<INotificationStrategy, RealTimeNotificationStrategy>();
+            services.AddTransient<IMediator, Mediator>();
+            services.AddRabbitMqEventBus(configuration);
+            services.AddHostedService<RabbitMQListener>();
+            services.AddTransient<INotificationSender, NotificationSender>();
             services.AddScoped<INotificationStrategy, PushNotificationStrategy>();
             services.AddScoped<INotificationStrategy, ScheduledNotificationStrategy>();
             services.AddScoped<INotificationStrategyFactory, NotificationStrategyFactory>();
@@ -36,6 +43,31 @@
 
         private static IServiceCollection AddConfigurations(this IServiceCollection services, IConfiguration configuration)
         {
+            services.Configure<EventBusOptions>(configuration.GetSection("EventBus"));
+
+            return services;
+        }
+
+        public static IServiceCollection AddRabbitMqEventBus(this IServiceCollection services, IConfiguration configuration)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = configuration["EventBus:HostName"],
+                UserName = configuration.GetValue<string>("EventBus:UserName"),
+                Password = configuration.GetValue<string>("EventBus:Password"),
+                DispatchConsumersAsync = true
+            };
+
+            var port = configuration.GetValue<int?>("EventBus:Port");
+            if (port.HasValue)
+            {
+                factory.Port = port.Value;
+            }
+
+            services.AddSingleton<IConnectionFactory>(factory);
+            services.AddSingleton<RabbitMQPersistentConnection>();
+            services.AddSingleton<IEventBus, RabbitMQEventBus>();
+
             return services;
         }
     }
